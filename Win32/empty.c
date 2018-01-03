148bf2792c15fa83ed96dde5eb206f9d7910e341
MohAhm <mad14002@student.mdh.se>
2018-01-03 22:05:00 +0100

    Show up dialog box

--- Win32/empty.c
+++ Win32/empty.c
@@ -1,11 +1,66 @@
 #include <windows.h>
 #include "resource.h"
 
+INT_PTR CALLBACK DialogProc(HWND, UINT, WPARAM, LPARAM);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) {
 
-	MessageBox(NULL, "It works man?\n", "A cool Mbox", 0);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) 
+{
+	HWND hWndDlg;
+	MSG msg;
 
-	return 1;
+	// Creating a modeless dialog box window
+	hWndDlg = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DialogProc);
+	if (hWndDlg == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
 
+	ShowWindow(hWndDlg, nCmdShow);
+	UpdateWindow(hWndDlg);
+
+	/* (the message processing loop that all windows applications must have) */
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+
+INT_PTR CALLBACK DialogProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		case WM_INITDIALOG:
+			return (INT_PTR)TRUE;
+		case WM_COMMAND:
+			switch (LOWORD(wParam))
+			{
+				case IDOK:
+					EndDialog(hDlg, LOWORD(wParam));
+					return (INT_PTR)TRUE;
+					break;
+				case IDCANCEL:
+					DestroyWindow(hDlg);
+					return (INT_PTR)TRUE;
+					break;
+				case ID_NY_KNAPP:
+					MessageBox(NULL, "It works man?\n", "A cool Mbox", 0);
+					return (INT_PTR)TRUE;
+					break;
+			}
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return (INT_PTR)TRUE;
+			break;
+		default:
+			return(DefWindowProc(hDlg, msg, wParam, lParam));
+	}
+
+	return (INT_PTR)FALSE;
 }
