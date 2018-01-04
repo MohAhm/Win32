0bced50831d9168ac2f553f1017fd71ce4346463
MohAhm <mad14002@student.mdh.se>
2018-01-04 20:58:01 +0100

    Dialog box without owner

--- Win32/empty.c
+++ Win32/empty.c
@@ -1,66 +1,67 @@
 #include <windows.h>
 #include "resource.h"
 
-INT_PTR CALLBACK DialogProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK MainDlgProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT WINAPI MainWndProc(HWND, UINT, WPARAM, LPARAM);
 
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
-	HWND hWndDlg;
+	HWND hDlgModeless;
 	MSG msg;
 
 	// Creating a modeless dialog box window
-	hWndDlg = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DialogProc);
-	if (hWndDlg == NULL)
+	hDlgModeless = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, MainDlgProc);
+
+	ShowWindow(hDlgModeless, nCmdShow);
+
+	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		MessageBox(NULL, "Window Creation Failed!", "Error!",
-			MB_ICONEXCLAMATION | MB_OK);
-		return 0;
-	}
-
-	ShowWindow(hWndDlg, nCmdShow);
-	UpdateWindow(hWndDlg);
-
-	/* (the message processing loop that all windows applications must have) */
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if (hDlgModeless == 0 || !IsDialogMessage(hDlgModeless, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
 
 	return msg.wParam;
 }
 
 
-INT_PTR CALLBACK DialogProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK MainDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-		case WM_INITDIALOG:
-			return (INT_PTR)TRUE;
-		case WM_COMMAND:
-			switch (LOWORD(wParam))
-			{
-				case IDOK:
-					EndDialog(hDlg, LOWORD(wParam));
-					return (INT_PTR)TRUE;
-					break;
-				case IDCANCEL:
-					DestroyWindow(hDlg);
-					return (INT_PTR)TRUE;
-					break;
-				case ID_NY_KNAPP:
-					MessageBox(NULL, "It works man?\n", "A cool Mbox", 0);
-					return (INT_PTR)TRUE;
-					break;
-			}
-			break;
-		case WM_DESTROY:
-			PostQuitMessage(0);
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
 			return (INT_PTR)TRUE;
 			break;
-		default:
-			return(DefWindowProc(hDlg, msg, wParam, lParam));
+		case IDCANCEL:
+			DestroyWindow(hDlg);
+			return (INT_PTR)TRUE;
+			break;
+		case ID_NY_KNAPP:
+			MessageBox(NULL, "It works man?\n", "A cool Mbox", 0);
+			return (INT_PTR)TRUE;
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return (INT_PTR)TRUE;
+		break;
+	default:
+		return(DefWindowProc(hDlg, msg, wParam, lParam));
 	}
 
 	return (INT_PTR)FALSE;
 }
+
+
+
+
+
